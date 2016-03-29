f117aaf2b8f62efc1f3582450ff1e8f498915f9a
valeinterfac <valeinterfac@yahoo.co.uk>
2016-03-29 21:41:31 +0100

    Tidy center_map, clear markers and boxes on reload

--- center_map.js
+++ center_map.js
@@ -1,58 +1,47 @@
 function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 8,
-          center: {lat: 50.82253, lng: -0.13716}
-        });
-        var geocoder = new google.maps.Geocoder();
-        var geocoder2 = new google.maps.Geocoder();
+ 	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 8,
+   		center: {lat: 50.82253, lng: -0.13716}
+ 	});
 
-        document.getElementById('submit').addEventListener('click', function() {
-          geocodeAddress(geocoder, geocoder2, map);
-        });
+	var marker1 = new google.maps.Marker;
+	var marker2 = new google.maps.Marker;
+
+	var bounds = new google.maps.LatLngBounds();
+
+	var geocoder1 = new google.maps.Geocoder();
+    var geocoder2 = new google.maps.Geocoder();
+
+    var address1, address2;
+
+	var latlong1 = document.getElementById('latlong1');
+	var latlong2 = document.getElementById('latlong2');
+	//Clear lat long box on load
+	latlong1.value = "";
+	latlong2.value = "";
+ 
+
+ 	document.getElementById('submit').addEventListener('click', function() {
+ 		address1 = document.getElementById('address1').value;
+		address2 = document.getElementById('address2').value;
+ 		geocodeAddress(geocoder2, map, address2, latlong2, marker1, function(addrs2) { bounds.extend(addrs2);});
+		geocodeAddress(geocoder1, map, address1, latlong1, marker2, function(addrs1) { bounds.extend(addrs1);});
+		map.fitBounds(bounds);
+	});
+}
+
+function geocodeAddress(Geocoder, resultsMap, address, latlong, marker, callback) {
+
+    Geocoder.geocode({'address': address}, function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+       		marker.setMap(resultsMap);
+           	marker.setPosition(results[0].geometry.location);
+            //bounds.extend(marker.position);
         }
-
-      function geocodeAddress(geocoder, geocoder2, resultsMap) {
-        var bounds = new google.maps.LatLngBounds();
-        var address = document.getElementById('address').value;
-        geocoder.geocode({'address': address}, function(results, status) {
-          if (status === google.maps.GeocoderStatus.OK) {
-            // resultsMap.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-              map: resultsMap,
-              position: results[0].geometry.location
-            });
-            bounds.extend(marker.position);
-          }
-          else {
-            alert('Geocode was not successful for the following reason: ' + status);
-          }
-          var latlong = document.getElementById('latlong');
-          var loc1 = results[0].geometry.location;
-          latlong.value = loc1;
-          // return loc1;
-        });
-
-        var address2 = document.getElementById('address2').value;
-        geocoder2.geocode({'address': address2}, function(results, status) {
-          if (status === google.maps.GeocoderStatus.OK) {
-            var marker = new google.maps.Marker({
-              map: resultsMap,
-              position: results[0].geometry.location
-            });
-            bounds.extend(marker.position);
-          }
-          else {
-            alert('Geocode was not successful for the following reason: ' + status);
-          }
-            var latlong2 = document.getElementById('latlong2');
-            var loc2 = results[0].geometry.location;
-            latlong2.value = loc2;
-            // return loc2;
-        });
-        // bounds.extend(loc1);
-        // bounds.extend(loc2);
-        var listener = google.maps.event.addListener(resultsMap, 'idle', function(bounds){
-          resultsMap.fitBounds(bounds);
-          google.maps.event.removeListener(listener);
-        });
-      }
+        else {
+             alert('Geocode was not successful for the following reason: ' + status);
+        }
+     	latlong.value = results[0].geometry.location;
+	  	callback(marker.position);
+	});	
+}
