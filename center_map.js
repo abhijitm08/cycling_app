fb06195c50818be197cedf90987c58d558ab1ee9
valeinterfac <valeinterfac@yahoo.co.uk>
2016-04-01 20:24:03 +0100

    Elevation data displayed

--- center_map.js
+++ center_map.js
@@ -1,3 +1,6 @@
+// Load the Visualization API and the columnchart package.
+google.load('visualization', '1', {packages: ['columnchart']});
+
 function initMap() {
 
   //initialise map
@@ -6,13 +9,13 @@ function initMap() {
    		center: {lat: 50.82253, lng: -0.13716}
  	});
 
+  var poly;
+
 	var marker1 = new google.maps.Marker;
 	var marker2 = new google.maps.Marker;
 
 	var geocoder1 = new google.maps.Geocoder();
   var geocoder2 = new google.maps.Geocoder();
-
-  // var bounds = new google.maps.LatLngBounds();
 
   var address1, address2;
 
@@ -29,6 +32,7 @@ function initMap() {
 		address2 = document.getElementById('address2').value;
     //Turn input into lat long, set markers
     var bounds = new google.maps.LatLngBounds();
+    //poly.setMap(null);
     success(geocoder2, map, address2, latlong2, marker2, geocoder1, address1, latlong1, marker1, bounds);
     });
 }
@@ -50,14 +54,71 @@ function geocodeAddress(Geocoder, resultsMap, address, latlong, marker, callback
 }
 
 function success(geocoder2, map, address2, latlong2, marker2, geocoder1, address1, latlong1, marker1, bounds){
+    var path = [];
+    // poly.setMap(null);
+    var elevator = new google.maps.ElevationService;
     geocodeAddress(geocoder2, map, address2, latlong2, marker2, function(position){
       console.log("callback called " + position); 
       bounds.extend(position);
+      path[0] = {lat: position.lat(), lng: position.lng()};
       geocodeAddress(geocoder1, map, address1, latlong1, marker1, function(position){
         console.log("callback called " + position); 
         bounds.extend(position);
-        console.log(bounds);
+        path[1] = {lat: position.lat(), lng: position.lng()};
+        // Draw the path, using the Visualization API and the Elevation service.
+        displayPathElevation(path, elevator, map);
+        //fit map to markers
         map.fitBounds(bounds);
       });
     });
   }
+
+function displayPathElevation(path, elevator, map) {
+  // Display a polyline of the elevation path.
+  poly = new google.maps.Polyline({
+    path: path,
+    strokeColor: '#0000CC',
+    opacity: 0.4,
+    map: map
+  });
+
+  // Create a PathElevationRequest object using this array.
+  // Ask for 256 samples along that path.
+  // Initiate the path request.
+  elevator.getElevationAlongPath({
+    'path': path,
+    'samples': 256
+  }, plotElevation);
+}
+
+// Takes an array of ElevationResult objects, draws the path on the map
+// and plots the elevation profile on a Visualization API ColumnChart.
+function plotElevation(elevations, status) {
+  var chartDiv = document.getElementById('elevation_chart');
+  if (status !== google.maps.ElevationStatus.OK) {
+    // Show the error code inside the chartDiv.
+    chartDiv.innerHTML = 'Cannot show elevation: request failed because ' +
+        status;
+    return;
+  }
+  // Create a new chart in the elevation_chart DIV.
+  var chart = new google.visualization.ColumnChart(chartDiv);
+
+  // Extract the data from which to populate the chart.
+  // Because the samples are equidistant, the 'Sample'
+  // column here does double duty as distance along the
+  // X axis.
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Sample');
+  data.addColumn('number', 'Elevation');
+  for (var i = 0; i < elevations.length; i++) {
+    data.addRow(['', elevations[i].elevation]);
+  }
+
+  // Draw the chart using the data within its DIV.
+  chart.draw(data, {
+    height: 150,
+    legend: 'none',
+    titleY: 'Elevation (m)'
+  });
+}
